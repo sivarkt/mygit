0aa4dd01ed86932b5fed3839ac08e9bb0dc7b9f2
sivarkt <sivarkt@gmail.com>
2023-09-13 12:03:02 +0530

    COLOR  CHAANGE

--- MyCalculator.java
+++ MyCalculator.java
@@ -40,7 +40,7 @@ super(frameText);
 
 int tempX=TOPX, y=TOPY;
 displayLabel.setBounds(tempX,y,240,HEIGHT);
-displayLabel.setBackground(Color.RED);
+displayLabel.setBackground(Color.GREEN);
 displayLabel.setForeground(Color.GREEN);
 add(displayLabel);
 
