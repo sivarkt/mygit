d641b0391b68b7a166c1e1dd60017eea571969f1
sivarkt <sivarkt@gmail.com>
2023-10-09 10:07:29 +0530

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Ic0b7ca64f9fb9c9617605d36cee61249c837a7ed

--- MyCalculator.java
+++ MyCalculator.java
@@ -41,7 +41,7 @@ super(frameText);
 int tempX=TOPX, y=TOPY;
 displayLabel.setBounds(tempX,y,240,HEIGHT);
 displayLabel.setBackground(Color.GREEN);
-displayLabel.setForeground(Color.BLUE);
+displayLabel.setForeground(Color.RED);
 add(displayLabel);
 
 memLabel.setBounds(TOPX,  TOPY+HEIGHT+ V_SPACE,WIDTH, HEIGHT);
